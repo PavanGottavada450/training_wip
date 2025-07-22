cf05648ad1861a946b60afd4d8fb3ac0e6ffa336
ramu <gottavadapavan@gmail.com>
2025-07-22 23:16:16 +0530

    commit changes

--- src/Day2/ReverseUsingStringBuffer.java
+++ src/Day2/ReverseUsingStringBuffer.java
@@ -4,7 +4,9 @@ public class ReverseUsingStringBuffer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		StringBuffer sb = new StringBuffer("Hello Java");
+        sb.reverse();
+        System.out.println("Reversed string: " + sb);
 	}
 
 }
