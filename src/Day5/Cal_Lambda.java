0f06b27e03c618dbc88fcb9a96072731a99b4d72
ramu <gottavadapavan@gmail.com>
2025-07-25 09:26:47 +0530

    added lambda program

---
+++ src/Day5/Cal_Lambda.java
@@ -0,0 +1,15 @@
+package Day5;
+
+interface cal{
+    int operator(int a, int b);
+}
+
+public class Cal_Lambda{
+    public static void main(String[] args){
+        cal add = (a,b) -> a + b;
+        cal sub = (a,b) -> a - b;
+        
+        System.out.println("Addition : " + add.operator(1,2));
+        System.out.print("Subtraction : " + sub.operator(5,3));
+    }
+}
---
+++ src/Day5/Main.java
@@ -0,0 +1,46 @@
+package Day5;
+
+
+abstract class Vehicle{
+    void startEngine(){
+       System.out.println("Engine Started"); 
+    }
+    abstract void drive();
+}
+
+interface Flyable{
+    void fly();
+}
+
+class Car1 extends Vehicle{
+    void drive(){
+        System.out.println("driving on road");
+    }
+}
+
+class Plane extends Vehicle implements Flyable{
+    void drive(){
+        System.out.println("Rolling on runway");
+    }
+    
+    public void fly(){
+        System.out.println("Flying in the air");
+    }
+}
+
+public class Main{
+    public static void main(String[] args){
+        Vehicle c = new Car1();
+        c.startEngine();
+        c.drive();
+        
+        Vehicle p = new Plane();
+        p.startEngine();
+        p.drive();
+        
+        // Cast to Flyable to call fly()
+        if (p instanceof Flyable) {
+            ((Flyable) p).fly();
+        }
+    }
+}
